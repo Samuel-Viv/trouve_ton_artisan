e1239e6e4aeb3696272fecb25089fc3a46d76fbe
Samuel VIVIER <samvivier25@gmail.com>
2024-04-11 15:01:37 +0200

    Add page services

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom";
-
+// pages
 import Header from "./asset/component/Header";
 import Home from "./asset/page/Home";
 import Batiment from './asset/page/Batiment';
+import Services from './asset/page/Service';
+
 function App() {
   return (
     <div className="App">
@@ -12,6 +14,7 @@ function App() {
       <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/Batiment" element={<Batiment/>}></Route>
+      <Route path="/Services" element={<Services/>}></Route>
       </Routes>
     </div>
   );
