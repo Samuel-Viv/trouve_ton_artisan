93514b525aff150cc3640fea63d9069a556b5b7b
Samuel VIVIER <samvivier25@gmail.com>
2024-04-11 14:52:22 +0200

    Add page batiment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Routes, Route} from "react-router-dom";
 
 import Header from "./asset/component/Header";
 import Home from "./asset/page/Home";
-
+import Batiment from './asset/page/Batiment';
 function App() {
   return (
     <div className="App">
@@ -11,6 +11,7 @@ function App() {
 
       <Routes>
       <Route path="/" element={<Home/>}></Route>
+      <Route path="/Batiment" element={<Batiment/>}></Route>
       </Routes>
     </div>
   );
